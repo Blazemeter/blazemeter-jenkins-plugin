389ff855185295a63e49338e66ab416166a643d3
dzmitrykashlach <dzmitrykashlach@gmail.com>
2015-01-22 21:41:07 +0300

    unzip jtlzip to workspace

--- src/main/java/hudson/plugins/blazemeter/utils/Utils.java
+++ src/main/java/hudson/plugins/blazemeter/utils/Utils.java
@@ -21,8 +21,10 @@ import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 /**
  * Created by dzmitrykashlach on 18/11/14.
@@ -284,29 +286,53 @@ public class Utils {
                     + "/" + filePath.getName() + "/" + session + ".zip");
             URL url=new URL(dataUrl);
             FileUtils.copyURLToFile(url, jtlZip);
+            unzip(jtlZip.getAbsolutePath(),jtlZip.getParent(),jenBuildLog);
         } catch (JSONException e) {
-            jenBuildLog.warn("Failed to get url to JTLZIP: ",e);
+            jenBuildLog.warn("Failed to get url to JTLZIP: ", e);
         } catch (MalformedURLException e) {
-            jenBuildLog.warn("Failed to create URL to JTLZIP: ",e);
+            jenBuildLog.warn("Failed to create URL to JTLZIP: ", e);
         } catch (IOException e) {
             jenBuildLog.warn("Failed to save JTLZIP: ",e);
         }
     }
-/*
 
-    public static void unzip(File file){
+    public static void unzip(String zipFilePath, String destDir,StdErrLog logger) {
+        File dir = new File(destDir);
+        // create output directory if it doesn't exist
+        if(!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+        //buffer for read and write data to file
+        byte[] buffer = new byte[1024];
         try {
-            ZipFile zipFile = new ZipFile(file.getCanonicalPath());
-
-            zipFile.extractAll(destination);
-        } catch (ZipException e) {
-            e.printStackTrace();
+            fis = new FileInputStream(zipFilePath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while(ze != null){
+                String fileName = ze.getName();
+                File newFile = new File(destDir + File.separator + fileName);
+                logger.info("Unzipping to "+newFile.getAbsolutePath());
+                //create directories for sub directories in zip
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                //close this ZipEntry
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            //close last ZipEntry
+            zis.closeEntry();
+            zis.close();
+            fis.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.warn("Failed to unzip file: ",e );
         }
+
     }
-    }
-*/
+
 
     public static Result validateLocalTresholds(TestResult testResult,
                                                 PerformanceBuilder builder,
