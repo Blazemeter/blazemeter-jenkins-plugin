410944a29a677edbe47dab422ac86b651325b2aa
dzmitrykashlach <dzmitrykashlach@gmail.com>
2015-01-27 13:45:16 +0300

    Utils.unzip() - extract only .jtl file

--- src/main/java/hudson/plugins/blazemeter/utils/Utils.java
+++ src/main/java/hudson/plugins/blazemeter/utils/Utils.java
@@ -350,43 +350,37 @@ public class Utils {
             Enumeration<? extends ZipEntry> zipFileEntries = zipFile.entries();
             while (zipFileEntries.hasMoreElements()) {
                 ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
-                jenBuildLog.info("\tExtracting entry: " + entry);
+                if(entry.getName().substring((entry.getName().length()-4)).equals(".jtl")&!entry.isDirectory()){
+                    jenBuildLog.info("\tExtracting jtl report: " + entry);
 
-                //create destination file
-                File destFile = new File(destDirectory, entry.getName());
+                    //create destination file
+                    File destFile = new File(destDirectory, entry.getName());
 
-                //create parent directories if needed
-                File parentDestFile = destFile.getParentFile();
-                parentDestFile.mkdirs();
+                    //create parent directories if needed
+                    File parentDestFile = destFile.getParentFile();
+                    parentDestFile.mkdirs();
 
-                if (!entry.isDirectory()) {
-                    bufIS = new BufferedInputStream(
-                            zipFile.getInputStream(entry));
-                    int currentByte;
+                        bufIS = new BufferedInputStream(
+                                zipFile.getInputStream(entry));
+                        int currentByte;
 
-                    // buffer for writing file
-                    byte data[] = new byte[BUFFER_SIZE];
+                        // buffer for writing file
+                        byte data[] = new byte[BUFFER_SIZE];
 
-                    // write the current file to disk
-                    FileOutputStream fOS = new FileOutputStream(destFile);
-                    BufferedOutputStream bufOS = new BufferedOutputStream(fOS, BUFFER_SIZE);
+                        // write the current file to disk
+                        FileOutputStream fOS = new FileOutputStream(destFile);
+                        BufferedOutputStream bufOS = new BufferedOutputStream(fOS, BUFFER_SIZE);
 
-                    while ((currentByte = bufIS.read(data, 0, BUFFER_SIZE)) != -1) {
-                        bufOS.write(data, 0, currentByte);
-                    }
+                        while ((currentByte = bufIS.read(data, 0, BUFFER_SIZE)) != -1) {
+                            bufOS.write(data, 0, currentByte);
+                        }
 
-                    // close BufferedOutputStream
-                    bufOS.flush();
-                    bufOS.close();
+                        // close BufferedOutputStream
+                        bufOS.flush();
+                        bufOS.close();
 
-                    // recursively unzip files
-                    if (entry.getName().toLowerCase().endsWith(ZIP_EXTENSION)) {
-                        String zipFilePath = destDirectory.getPath() + File.separatorChar + entry.getName();
-
-                        unzip(zipFilePath, zipFilePath.substring(0,
-                                zipFilePath.length() - ZIP_EXTENSION.length()), jenBuildLog);
-                    }
                 }
+
             }
             bufIS.close();
         } catch (Exception e) {
