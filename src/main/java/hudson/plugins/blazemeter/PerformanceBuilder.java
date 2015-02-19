e806bae82a8670925035b1f0a72e069607a2924a
zmicer <dzmitrykashlach@gmail.com>
2015-02-19 13:31:15 +0300

    app/#report -> app/#reports

--- src/main/java/hudson/plugins/blazemeter/PerformanceBuilder.java
+++ src/main/java/hudson/plugins/blazemeter/PerformanceBuilder.java
@@ -318,7 +318,7 @@ public class PerformanceBuilder extends Builder {
                JSONObject startJO = (JSONObject) json.get("result");
                session = ((JSONArray) startJO.get("sessionsId")).get(0).toString();
                APIFactory apiFactory = APIFactory.getApiFactory();
-               jenBuildLog.info("Blazemeter test report will be available at " + apiFactory.getBlazeMeterUrl() + "/app/#report/" + session + "/loadreport");
+               jenBuildLog.info("Blazemeter test report will be available at " + apiFactory.getBlazeMeterUrl() + "app/#reports/" + session + "/loadreport");
                jenBuildLog.info("Blazemeter test log will be available at " + build.getLogFile().getParent() + "/" + Constants.BZM_JEN_LOG);
            }
 
