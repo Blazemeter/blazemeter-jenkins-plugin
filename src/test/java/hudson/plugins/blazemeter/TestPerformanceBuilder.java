754d4b4df22a1b070438af71f50f8d4faf0c0756
zmicer <dzmitrykashlach@gmail.com>
2015-07-07 11:37:15 +0300

    TestPerformanceBuilder:configure_save()

--- src/test/java/hudson/plugins/blazemeter/TestPerformanceBuilder.java
+++ src/test/java/hudson/plugins/blazemeter/TestPerformanceBuilder.java
@@ -1,10 +1,14 @@
 package hudson.plugins.blazemeter;
 
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import hudson.model.FreeStyleProject;
+import hudson.plugins.blazemeter.utils.Constants;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+
+import java.util.List;
 
 
 /**
@@ -14,17 +18,17 @@ public class TestPerformanceBuilder {
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
-    @Ignore
     @Test
-    public void first() throws Exception {
+    public void configure_save() throws Exception {
         FreeStyleProject project = j.createFreeStyleProject();
-        PerformanceBuilder pbBefore=new PerformanceBuilder("1234567890","60","","","123","v3","",
-                                                                      "",false,"","","","");
-//        j.jenkins
+        PerformanceBuilder pbBefore=new PerformanceBuilder("1234567890","60","mainJmx","dataFolder", Constants.CREATE_BZM_TEST_NOTE,
+                                                                      "v3",Constants.USE_TEST_LOCATION,
+                                                                      "pathToJsonConfiguration",false,"1","2","3","4");
         project.getBuildersList().add(pbBefore);
-//        j.submit(j.createWebClient().getPage(project,"configure").getForms().get(0));
-//        PerformanceBuilder pbAfter=project.getBuildersList().get(PerformanceBuilder.class);
-
-//        j.assertEqualBeans(pbBefore,pbAfter,"prop1,prop2,prop3,...");
+        List<HtmlForm> htmlForms= j.createWebClient().getPage(project,"configure").getForms();
+        j.submit(htmlForms.get(1));
+        PerformanceBuilder pbAfter=project.getBuildersList().get(PerformanceBuilder.class);
+        j.assertEqualBeans(pbBefore,pbAfter,"jobApiKey,testDuration,testId,location,jsonConfig,useServerTresholds,errorFailedThreshold," +
+                                             "errorUnstableThreshold,responseTimeFailedThreshold,responseTimeUnstableThreshold");
     }
 }
