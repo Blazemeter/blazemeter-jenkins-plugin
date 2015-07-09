6c814da6ee22df7d7f1a7347df0f7cd5621906cd
zmicer <dzmitrykashlach@gmail.com>
2015-07-09 16:56:06 +0300

    TestBzmServiceManager:validateUserKey(+,-,!),getVersion

--- src/test/java/hudson/plugins/blazemeter/TestBzmServiceManager.java
+++ src/test/java/hudson/plugins/blazemeter/TestBzmServiceManager.java
@@ -1,103 +1,65 @@
 package hudson.plugins.blazemeter;
 
 import hudson.plugins.blazemeter.utils.BzmServiceManager;
-import org.junit.Assert;
-import org.apache.commons.io.FileUtils;
+import hudson.util.FormValidation;
+import org.junit.*;
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockserver.client.server.MockServerClient;
-import org.mockserver.junit.MockServerRule;
-import org.mockserver.model.Parameter;
-
-import java.io.File;
 import java.io.IOException;
-
-import static org.mockserver.matchers.Times.once;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
-import static org.mockserver.model.StringBody.exact;
 
 /**
  * Created by zmicer on 8.7.15.
  */
 public class TestBzmServiceManager {
 
+    @BeforeClass
+    public static void setUp()throws IOException{
+        MockedAPIConfigurator.startMockedAPIServer();
+    }
 
-    private String mockedUserKey ="mockedUserKey";
-    private int mockedApiPort=1234;
-    private String mockedApiUrl="http://127.0.0.1:"+mockedApiPort;
-    private String mockedApiHost ="127.0.0.1";
-
-    @Rule
-    public MockServerRule mockServerRule = new MockServerRule(mockedApiPort, mockedApiHost);
-
-    private MockServerClient mockServerClient = new MockServerClient(mockedApiHost,mockedApiPort);
+    @AfterClass
+    public static void tearDown()throws IOException{
+        MockedAPIConfigurator.stopMockedAPIServer();
+    }
 
     @Test
     public void getUserEmail_positive() throws IOException,JSONException{
-        File jsonFile = new File(TestConstants.RESOURCES + "/getUserEmail_positive.json");
-        String userProfile=FileUtils.readFileToString(jsonFile);
-
-        mockServerClient.when(
-                request()
-                        .withMethod("GET")
-                        .withPath("/api/latest/user")
-                        .withHeader("Accept", "application/json")
-                        .withQueryStringParameters(
-                                new Parameter("api_key", mockedUserKey)
-                        ),
-                once()
-        )
-                .respond(
-                        response().withHeader("application/json")
-                                .withStatusCode(200).withBody(userProfile));
-        String email=BzmServiceManager.getUserEmail(mockedUserKey, mockedApiUrl);
+        String email=BzmServiceManager.getUserEmail(TestConstants.MOCKED_USER_KEY_VALID, TestConstants.mockedApiUrl);
         Assert.assertEquals(email,"dzmitry.kashlach@blazemeter.com");
     }
 
     @Test
     public void getUserEmail_negative() throws IOException,JSONException{
-        File jsonFile = new File(TestConstants.RESOURCES + "/getUserEmail_negative.json");
-        String userProfile=FileUtils.readFileToString(jsonFile);
-
-        mockServerClient.when(
-                request()
-                        .withMethod("GET")
-                        .withPath("/api/latest/user")
-                        .withHeader("Accept", "application/json")
-                        .withQueryStringParameters(
-                                new Parameter("api_key", mockedUserKey)
-                        ),
-                once()
-        )
-                .respond(
-                        response().withHeader("application/json")
-                                .withStatusCode(200).withBody(userProfile));
-        String email=BzmServiceManager.getUserEmail(mockedUserKey, mockedApiUrl);
+        String email=BzmServiceManager.getUserEmail(TestConstants.MOCKED_USER_KEY_INVALID, TestConstants.mockedApiUrl);
         Assert.assertEquals(email,"");
     }
+
     @Test
     public void getUserEmail_exception() throws IOException,JSONException{
-        File jsonFile = new File(TestConstants.RESOURCES + "/getUserEmail_jexception.txt");
-        String userProfile=FileUtils.readFileToString(jsonFile);
-
-        mockServerClient.when(
-                request()
-                        .withMethod("GET")
-                        .withPath("/api/latest/user")
-                        .withHeader("Accept", "application/json")
-                        .withQueryStringParameters(
-                                new Parameter("api_key", mockedUserKey)
-                        ),
-                once()
-        )
-                .respond(
-                        response().withHeader("application/json")
-                                .withStatusCode(200).withBody(userProfile));
-        String email=BzmServiceManager.getUserEmail(mockedUserKey, mockedApiUrl);
+        String email=BzmServiceManager.getUserEmail(TestConstants.MOCKED_USER_KEY_EXCEPTION, TestConstants.mockedApiUrl);
         Assert.assertEquals(email,"");
+    }
+
+    @Test
+    public void validateUserKey_positive() throws IOException,JSONException{
+        FormValidation validation=BzmServiceManager.validateUserKey(TestConstants.MOCKED_USER_KEY_VALID, TestConstants.mockedApiUrl);
+        Assert.assertEquals(validation.kind, FormValidation.Kind.OK);
+    }
+
+    @Test
+    public void validateUserKey_negative() throws IOException,JSONException{
+        FormValidation validation=BzmServiceManager.validateUserKey(TestConstants.MOCKED_USER_KEY_INVALID, TestConstants.mockedApiUrl);
+        Assert.assertEquals(validation.kind, FormValidation.Kind.ERROR);
+    }
+
+    @Test
+    public void validateUserKey_exception() throws IOException,JSONException{
+        FormValidation validation=BzmServiceManager.validateUserKey(TestConstants.MOCKED_USER_KEY_EXCEPTION, TestConstants.mockedApiUrl);
+        Assert.assertEquals(validation.kind, FormValidation.Kind.ERROR);
+    }
+
+    @Test
+    public void getVersion() throws IOException,JSONException{
+        String version=BzmServiceManager.getVersion();
+        Assert.assertTrue(version.matches("^(\\d{1,}\\.+\\d{1,2}\\S*)$"));
     }
 }
--- src/test/java/hudson/plugins/blazemeter/TestConstants.java
+++ src/test/java/hudson/plugins/blazemeter/TestConstants.java
@@ -5,4 +5,13 @@ package hudson.plugins.blazemeter;
  */
 public interface TestConstants {
     String RESOURCES = System.getProperty("user.dir")+"/src/test/java/hudson/plugins/blazemeter/resources";
+
+
+    // Mocked API constants
+    String MOCKED_USER_KEY_VALID ="mockedAPIKeyValid";
+    String MOCKED_USER_KEY_INVALID ="mockedAPIKeyInValid";
+    String MOCKED_USER_KEY_EXCEPTION ="mockedAPIKeyException";
+    int mockedApiPort=1234;
+    String mockedApiUrl="http://127.0.0.1:"+mockedApiPort;
+    String mockedApiHost ="127.0.0.1";
 }
