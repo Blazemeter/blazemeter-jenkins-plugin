5024040f51c68561ee96625e219a21a285e6a46d
zmicer <dzmitrykashlach@gmail.com>
2016-09-22 18:21:27 +0300

    JEN-178:remove unused import

--- src/main/java/hudson/plugins/blazemeter/api/ApiV3Impl.java
+++ src/main/java/hudson/plugins/blazemeter/api/ApiV3Impl.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Logger;
 
 
 public class ApiV3Impl implements Api {
