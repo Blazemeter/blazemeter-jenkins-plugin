c490fba822a77402b317fc90e28cfb9d89ac973f
zmicer <dzmitrykashlach@gmail.com>
2016-10-19 15:02:21 +0300

    JEN-187 JEN-152: reverted credentials behavior;

--- src/main/java/hudson/plugins/blazemeter/BlazemeterCredentialImpl.java
+++ src/main/java/hudson/plugins/blazemeter/BlazemeterCredentialImpl.java
@@ -14,9 +14,9 @@
 
 package hudson.plugins.blazemeter;
 
+import com.cloudbees.plugins.credentials.BaseCredentials;
 import com.cloudbees.plugins.credentials.CredentialsDescriptor;
 import com.cloudbees.plugins.credentials.CredentialsScope;
-import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
 import hudson.Extension;
 import hudson.plugins.blazemeter.utils.Constants;
 import hudson.plugins.blazemeter.utils.JobUtility;
@@ -26,34 +26,34 @@ import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
-
 import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
-public class BlazemeterCredentialImpl extends BaseStandardCredentials{
+public class BlazemeterCredentialImpl extends BaseCredentials {
     private static final long serialVersionUID = 1L;
 
     private String apiKey =null;
+    private String description=null;
 
     @DataBoundConstructor
-    public BlazemeterCredentialImpl(String id, String apiKey,String description) {
-        super(CredentialsScope.GLOBAL,id,description);
+    public BlazemeterCredentialImpl(String apiKey,String description) {
+        super(CredentialsScope.GLOBAL);
         this.apiKey=apiKey;
+        this.description=description;
     }
 
     public String getId() {
-        if (super.getId() == null) {
-            return StringUtils.left(apiKey, 4) + Constants.THREE_DOTS + StringUtils.right(apiKey, 4);
-        } else {
-            return super.getId();
-        }
+        return StringUtils.left(apiKey,4) + Constants.THREE_DOTS + StringUtils.right(apiKey, 4);
     }
 
     public String getApiKey() {
         return apiKey;
     }
 
+    public String getDescription() {
+        return description;
+    }
 
 
     @Extension
