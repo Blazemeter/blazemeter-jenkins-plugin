d732e90f160e36bd6400b00ce29308cc14db1754
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-12-19 14:04:45 +0300

    add bzm utils from blazemeter-api-client (#13)

---
+++ src/main/java/hudson/plugins/blazemeter/utils/BzmUtils.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2017 BlazeMeter Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hudson.plugins.blazemeter.utils;
+
+import com.blazemeter.api.logging.Logger;
+import com.blazemeter.api.logging.UserNotifier;
+import com.blazemeter.api.utils.BlazeMeterUtils;
+
+public class BzmUtils extends BlazeMeterUtils {
+
+    private static final String APP_KEY = "app_key=jnk100x987c06f4e10c4";
+
+    private static final String CLIENT_IDENTIFICATION = "&_clientId=CI_JENKINS&_clientVersion="
+            + JobUtility.version();
+
+    public static String JENKINS_PLUGIN_INFO = APP_KEY + CLIENT_IDENTIFICATION;
+
+    public BzmUtils(String apiKeyId, String apiKeySecret, String address, UserNotifier notifier, Logger logger) {
+        super(apiKeyId, apiKeySecret, address, "", notifier, logger);
+    }
+
+    @Override
+    protected String modifyRequestUrl(String url) {
+        return url.contains("?") ?
+                (url + '&' + JENKINS_PLUGIN_INFO) :
+                (url + '?' + JENKINS_PLUGIN_INFO);
+    }
+}
