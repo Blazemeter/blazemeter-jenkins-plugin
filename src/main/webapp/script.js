9874390ff1f8530481e986d3ed52c3bdf6d8b0f9
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-01-31 15:38:46 +0300

    add select after press enter

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -207,15 +207,14 @@ function selectLast(liList) {
 }
 
 function setCurrentLi() {
-    console.log("select");
-    if (curFocusedLi == null) {
-
+    if (curFocusedLi != null) {
+        onClickListElementHandler(curFocusedLi);
     }
 }
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var keys = {37: 1, 38: 1, 39: 1, 40: 1, 13: 1};
 
 function preventDefault(e) {
     e = e || window.event;
