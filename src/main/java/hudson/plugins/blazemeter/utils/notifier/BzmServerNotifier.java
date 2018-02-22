3002f9bbc43a7715618ba2834fe01d94db7aaf48
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-02-22 14:56:08 +0300

    update licenses

--- src/main/java/hudson/plugins/blazemeter/utils/notifier/BzmServerNotifier.java
+++ src/main/java/hudson/plugins/blazemeter/utils/notifier/BzmServerNotifier.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 BlazeMeter Inc.
+ * Copyright 2018 BlazeMeter Inc.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
