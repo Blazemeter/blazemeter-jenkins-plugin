abd1f79a4fc4ae798f5a9884c08792a6a1532f1c
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-06-05 16:58:41 +0300

    Fix/double build steps (#63)
    * fix some styles and scripts
    
    * fix scripts
    
    * fix more scripts
    
    * fix scripts
    
    * fix move scroll
    
    * cleanup
    
    * fix search
    
    * fix move scroll
    
    * cleanup logs

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,8 +1,14 @@
-function onChangeSelectHandler() {
-    var selectEl = getSelectEl();
+function onChangeSelectHandler(event) {
+    var target = event.target;
+    var testDivEl = target.closest(".testDiv");
+    onChangeSelectHandlerForTestDiv(testDivEl);
+};
+
+function onChangeSelectHandlerForTestDiv(testDiv) {
+    var selectEl = getSelectElForTestDiv(testDiv);
     var options = selectEl.options;
 
-    var generatedUlDivEl = document.getElementById("generatedUlDiv");
+    var generatedUlDivEl = testDiv.querySelector(".generatedUlDiv");
     var generatedUl = '<ul id="generatedUl" data-display-elements="' + options.length + '">';
     for (i = 0; i < options.length; i++) {
         generatedUl += '<li class="generatedLi" onclick="onClickListElementHandler(this)" '
@@ -13,34 +19,36 @@ function onChangeSelectHandler() {
                     + '" >' + options[i].label + '</li>';
 
         if (options[i].selected) {
-            setNameToResult(options[i].label);
+            setNameToResultFromTestDiv(testDiv, options[i].label);
         }
     }
     generatedUl += '</ul>';
     generatedUlDivEl.innerHTML = generatedUl;
 
-    var searchInputDivEl = document.getElementById("searchInputDiv");
-    searchInputDivEl.innerHTML = '<input type="text" onkeyup="onKeyUpSearch()" id="searchInput" name="searchInput" value="" class="setting-input" placeholder="Search Tests..."/>';
+    var searchInputDivEl = testDiv.querySelector(".searchInputDiv");
+    searchInputDivEl.innerHTML = '<input type="text" onkeyup="onKeyUpSearch(this)" id="searchInput" name="searchInput" value="" class="setting-input" placeholder="Search Tests..."/>';
 };
 
 // START
 setTimeout(function() {
-    var selectEl = getSelectEl();
-    setNameToResult(selectEl.getAttribute("value"));
+    var testDivs = getAllTestDivs();
+    for (var i = 0; i < testDivs.length; i++) {
+        var div = testDivs[i];
+        setNameToResultFromTestDiv(div, getSelectElForTestDiv(div).getAttribute("value"));
+        executeIntervalTaskForDiv(div);
+    }
 
-    executeIntervalTask();
 }, 2000);
 // end of START
 
-function executeIntervalTask() {
+function executeIntervalTaskForDiv(testDiv) {
     // waiting when init state of original <select> will be changed
     var count = 0;
     var interval = setInterval(function() {
-
-        var resultDivEl = document.getElementById("result");
+        var resultDivEl = testDiv.querySelector("#result");
         var prevValue = resultDivEl.innerHTML;
 
-        onChangeSelectHandler(); // if <select> element filled it will change 'resultDivEl.innerHTML'
+        onChangeSelectHandlerForTestDiv(testDiv); // if <select> element filled it will change 'resultDivEl.innerHTML'
         var curValue = resultDivEl.innerHTML;
 
         if ((prevValue != curValue) || (count >= 20)) {
@@ -50,16 +58,31 @@ function executeIntervalTask() {
     }, 500);
 };
 
-function getSelectEl() {
-    var testDivEl = document.getElementById("testDiv");
-    return testDiv.getElementsByTagName("select")[0];
+function setNameToResultFromTestDiv(testDiv, name) {
+    var resultDivEl = testDiv.querySelector("#result");
+    resultDivEl.innerHTML = name;
+};
+
+function getAllTestDivs() {
+    return document.getElementsByClassName("testDiv");
+};
+
+function executeIntervalTask(selectEl) {
+    var builderEl = selectEl.closest("div[descriptorid='hudson.plugins.blazemeter.PerformanceBuilder']");
+    var testDivEl = builderEl.querySelector(".testDiv");
+    executeIntervalTaskForDiv(testDivEl)
 };
 
 
-function onKeyUpSearch() {
-    var searchInputEl = document.getElementById("searchInput");
+function getSelectElForTestDiv(testDivEl) {
+    return testDivEl.getElementsByTagName("select")[0];
+};
+
+
+function onKeyUpSearch(searchInputEl) {
     var text = searchInputEl.value.toLowerCase();
-    var ulEl = document.getElementById("generatedUl");
+    var testDivEl = searchInputEl.closest(".testDiv");
+    var ulEl = testDivEl.querySelector("#generatedUl");
     var liList = ulEl.getElementsByTagName("li");
     var displayIndex = 0;
     for (i = 0; i < liList.length; i++) {
@@ -80,46 +103,19 @@ function onKeyUpSearch() {
     }
 };
 
-function onClickResultHandler() {
-    var toggleEl = document.getElementById("hiddenSelect");
+function onClickResultHandler(resultDivEl) {
+    var testDivEl = resultDivEl.closest(".testDiv");
+    var toggleEl = testDivEl.querySelector(".hiddenSelect");
     if (toggleEl.style.display === "block") {
-        hideHiddenSelect();
+        hideHiddenSelect(toggleEl);
     } else {
-        onChangeSelectHandler();
-        openHiddenSelect(toggleEl);
+        onChangeSelectHandlerForTestDiv(testDivEl);
+        openHiddenSelect(testDivEl, toggleEl);
         disableScroll();
     }
 };
 
-function openHiddenSelect(toggleEl) {
-    toggleEl.style.display = "block";
-    var searchInputEl = document.getElementById("searchInput");
-    searchInputEl.focus();
-    var liList = document.getElementById("generatedUl").getElementsByTagName("li");
-    var resultDivEl = document.getElementById("result");
-    for (i = 0; i < liList.length; i++) {
-        if (liList[i].innerHTML == resultDivEl.innerHTML) {
-            focusLi(liList[i]);
-            moveScroll(liList[i].dataset.displayIndex);
-            return;
-        }
-    }
-};
-
-function onClickListElementHandler(li) {
-    setNameToResult(li.innerHTML);
-    setValueToSelect(li);
-    hideHiddenSelect();
-};
-
-document.onmousedown = function(event) {
-    if (!isGeneratedElement(event)) {
-        hideHiddenSelect();
-    }
-};
-
-function hideHiddenSelect() {
-    var toggleEl = document.getElementById("hiddenSelect");
+function hideHiddenSelect(toggleEl) {
     if (toggleEl != null) {
         toggleEl.style.display = "none";
         curFocusedLi = null;
@@ -127,16 +123,49 @@ function hideHiddenSelect() {
     }
 };
 
-function setValueToSelect(li) {
-    var selectEl = getSelectEl();
+
+function openHiddenSelect(testDivEl, toggleEl) {
+    toggleEl.style.display = "block";
+    var searchInputEl = toggleEl.querySelector("#searchInput");
+    searchInputEl.focus();
+    var generatedUlEl = toggleEl.querySelector("#generatedUl");
+    var liList = generatedUlEl.getElementsByTagName("li");
+    var resultDivEl = testDivEl.querySelector("#result");
+    for (i = 0; i < liList.length; i++) {
+        if (liList[i].innerHTML == resultDivEl.innerHTML) {
+            focusLi(liList[i]);
+            moveScroll(generatedUlEl, liList[i].dataset.displayIndex);
+            return;
+        }
+    }
+};
+
+function onClickListElementHandler(li) {
+    var testDivEl = li.closest(".testDiv");
+    setNameToResultFromTestDiv(testDivEl, li.innerHTML);
+    setValueToSelect(testDivEl, li);
+    var toggleEl = testDivEl.querySelector(".hiddenSelect");
+    hideHiddenSelect(toggleEl);
+};
+
+document.onmousedown = function(event) {
+    if (!isGeneratedElement(event)) {
+        var testDivs = getAllTestDivs();
+        for (var i = 0; i < testDivs.length; i++) {
+            var div = testDivs[i];
+            var toggleEl = div.querySelector(".hiddenSelect");
+            hideHiddenSelect(toggleEl);
+        }
+    }
+};
+
+
+function setValueToSelect(testDivEl, li) {
+    var selectEl = getSelectElForTestDiv(testDivEl);
     selectEl.setAttribute("value", li.dataset.value);
     selectEl.options[li.dataset.index].selected = true;
 };
 
-function setNameToResult(name) {
-    var resultDivEl = document.getElementById("result");
-    resultDivEl.innerHTML = name;
-};
 
 function isGeneratedElement(event) {
     return (event.target.id == "result"
@@ -148,14 +177,16 @@ function isGeneratedElement(event) {
 document.onkeydown = function(event) {
     var charCode = event.keyCode;
     var focusedTag = document.activeElement.tagName;
-    var toggleEl = document.getElementById("hiddenSelect");
-    if (toggleEl.style.display === "block") {
+    var targetInputEl = event.target;
+    var toggleEl = targetInputEl.closest(".hiddenSelect");
+    if (toggleEl != null && toggleEl.style.display === "block") {
+        var generatedUlEl = toggleEl.querySelector("#generatedUl");
         if (isPressedDown(charCode)) {
-            selectNextLi();
+            selectNextLi(generatedUlEl);
         } else if (isPressedUp(charCode)) {
-            selectPrevLi();
+            selectPrevLi(generatedUlEl);
         } else if (isPressedEnter(charCode)) {
-            setCurrentLi();
+            setCurrentLi(generatedUlEl);
         }
         preventDefaultForScrollKeys(event);
     }
@@ -192,29 +223,27 @@ function clearFocusLi(li) {
     li.style.color = "#000000";
 };
 
-function selectNextLi() {
+function selectNextLi(ulEl) {
     var currentIndex = (curFocusedLi == null) ? -1 : curFocusedLi.dataset.displayIndex;
     var nextIndex = ++currentIndex;
-    var ulEl = document.getElementById("generatedUl");
     var liList = ulEl.getElementsByTagName("li");
     for (i = 0; i < liList.length; i++) {
         if (liList[i].dataset.displayIndex == nextIndex) {
             focusLi(liList[i]);
-            moveScroll(nextIndex);
+            moveScroll(ulEl, nextIndex);
             return;
         }
     }
-    selectFirst(liList);
+    selectFirst(ulEl, liList);
 };
 
 
 // need use liList[i].dataset.displayIndex
-function moveScroll(index) {
-    var ulEl = document.getElementById("generatedUl");
+function moveScroll(ulEl, index) {
     var elementsCount = ulEl.dataset.displayElements;
     var liList = ulEl.getElementsByTagName("li");
     if (liList.length > 0 && elementsCount > 0) {
-        var liHeight = parseFloat(window.getComputedStyle(liList[0], null).getPropertyValue("height"));
+        var liHeight = parseFloat(getLiHeight(liList, index));
         var ulHeight = parseFloat(window.getComputedStyle(ulEl, null).getPropertyValue("height"));
         var median = (ulHeight / liHeight / 2);
         if (index > median) {
@@ -225,20 +254,35 @@ function moveScroll(index) {
     }
 };
 
-function selectFirst(liList) {
+function getLiHeight(liList, displayIndex) {
+    for (i = 0; i < liList.length; i++) {
+        if (liList[i].dataset.displayIndex == displayIndex) {
+            return liList[i].clientHeight;
+        }
+    }
+
+    var height = window.getComputedStyle(liList[0], null).getPropertyValue("height");
+    if (height == 'auto') {
+        return 19;
+    } else {
+        return height;
+    }
+};
+
+function selectFirst(ulEl, liList) {
     for (i = 0; i < liList.length; i++) {
         if (liList[i].dataset.displayIndex != -1) {
             focusLi(liList[i]);
-            moveScroll(liList[i].dataset.displayIndex);
+            moveScroll(ulEl, liList[i].dataset.displayIndex);
             return;
         }
     }
 };
 
-function selectPrevLi() {
-    var liList = document.getElementById("generatedUl").getElementsByTagName("li");
+function selectPrevLi(ulEl) {
+    var liList = ulEl.getElementsByTagName("li");
     if (curFocusedLi == null || curFocusedLi.dataset.displayIndex <= 0) {
-        selectLast(liList);
+        selectLast(ulEl, liList);
         return;
     }
     var currentIndex = curFocusedLi.dataset.displayIndex;
@@ -246,24 +290,24 @@ function selectPrevLi() {
     for (i = 0; i < liList.length; i++) {
         if (liList[i].dataset.displayIndex == prevIndex) {
             focusLi(liList[i]);
-            moveScroll(prevIndex);
+            moveScroll(ulEl, prevIndex);
             return;
         }
     }
-    selectLast(liList);
+    selectLast(ulEl, liList);
 };
 
-function selectLast(liList) {
+function selectLast(ulEl, liList) {
     for (i = liList.length - 1; i >= 0; i--) {
         if (liList[i].dataset.displayIndex != -1) {
             focusLi(liList[i]);
-            moveScroll(liList[i].dataset.displayIndex);
+            moveScroll(ulEl, liList[i].dataset.displayIndex);
             return;
         }
     }
 };
 
-function setCurrentLi() {
+function setCurrentLi(ulEl) {
     if (curFocusedLi != null) {
         onClickListElementHandler(curFocusedLi);
     }
