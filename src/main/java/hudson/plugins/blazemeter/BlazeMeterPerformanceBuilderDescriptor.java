a132dfa332218232d2f81790c9ff973fededfa17
Puneet Jain <105224306+pjain05@users.noreply.github.com>
2025-11-13 16:40:15 +0530

    MOB-44067: Add permission check

--- src/main/java/hudson/plugins/blazemeter/BlazeMeterPerformanceBuilderDescriptor.java
+++ src/main/java/hudson/plugins/blazemeter/BlazeMeterPerformanceBuilderDescriptor.java
@@ -137,12 +137,6 @@ public class BlazeMeterPerformanceBuilderDescriptor extends BuildStepDescriptor<
                                           @QueryParameter("workspaceId") String workspaceId,
                                           @QueryParameter("testId") String testId) throws FormValidation {
 
-        Item item = Stapler.getCurrentRequest().findAncestorObject(Item.class);
-        if (item != null) {
-            item.checkPermission(Item.READ);
-        } else {
-            Jenkins.get().checkPermission(Jenkins.READ);
-        }
         ListBoxModel items = new ListBoxModel();
 
         try {
@@ -248,13 +242,6 @@ public class BlazeMeterPerformanceBuilderDescriptor extends BuildStepDescriptor<
     @RequirePOST
     public ListBoxModel doFillWorkspaceIdItems(@QueryParameter("credentialsId") String credentialsId,
                                                @QueryParameter("workspaceId") String workspaceId) throws FormValidation {
-        Item item = Stapler.getCurrentRequest().findAncestorObject(Item.class);
-        if (item != null) {
-            item.checkPermission(Item.READ);
-        } else {
-            Jenkins.get().checkPermission(Jenkins.READ);
-        }
-
         ListBoxModel items = new ListBoxModel();
 
         try {
@@ -308,16 +295,10 @@ public class BlazeMeterPerformanceBuilderDescriptor extends BuildStepDescriptor<
 
     @RequirePOST
     public ListBoxModel doFillCredentialsIdItems(@QueryParameter("credentialsId") String credentialsId) {
-
-        Item item = Stapler.getCurrentRequest().findAncestorObject(Item.class);
-        if (item != null) {
-            item.checkPermission(Item.CONFIGURE);
-        } else {
-            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
-        }
-
         ListBoxModel items = new ListBoxModel();
         try {
+
+            Item item = Stapler.getCurrentRequest().findAncestorObject(Item.class);
             List<BlazemeterCredentialsBAImpl> credentials =
                     CredentialsProvider.lookupCredentials(
                             BlazemeterCredentialsBAImpl.class,
